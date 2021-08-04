d886fcc7eb4f70d3d150f4bd14ef409510d22949
Ayushzz <ayushguptadon@gmail.com>
2021-08-04 16:52:48 +0530

    deploy my react app to github

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Students & Writers mostly look for some questions when coming to paragraph writing about any topic or thing or person. The questions raised by most of the students while thinking about writing a paragraph are Paragraph Writing Examples, What is the perfect paragraph format? How many steps involved to write a paragraph? How to write a good paragraph? How many sentences are included in a para? and many more like these.</p>
+        </div>
+    )
+}
